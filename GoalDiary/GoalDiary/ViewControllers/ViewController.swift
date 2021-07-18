9fb4e257bc7e21574ae410184ac4f1c3bf8c3864
Furkan Hanci <87238473+FurkanHanciSecond@users.noreply.github.com>
2021-07-18 14:40:17 +0300

    Add files via upload

---
+++ GoalDiary/GoalDiary/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalDiary
+//
+//  Created by Furkan Hanci on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.makeGradient(isVertical: true, colorArray: [.bg1 , .bg2])
+    }
+
+
+}
+
